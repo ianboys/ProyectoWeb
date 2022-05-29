4bf7eea4a32e3fbaa24d9b088e147b34a766bc1d
ianboys <ian-boys@hotmail.com>
2022-05-29 17:59:09 -0600

    Empezado componente orden, productos completos

--- ProyectoAngular/src/app/orden-component/orden-component.component.ts
+++ ProyectoAngular/src/app/orden-component/orden-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from '../modelos/producto.model';
+import { ProductosService } from '../servicios/productos.service';
 
 @Component({
   selector: 'app-orden-component',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orden-component.component.css']
 })
 export class OrdenComponentComponent implements OnInit {
+  productos:Producto[]=[];
+  titulo = "Orden Nueva";
+  id?: string;
 
-  constructor() { }
+  constructor(private productoService:ProductosService) { }
 
   ngOnInit(): void {
+    this.obtenerProductos();
+  }
+
+  obtenerProductos(){
+    this.productoService.obtenerProductos().subscribe(doc => {
+      this.productos = [];
+      doc.forEach((element: any) => {
+        this.productos.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.productos);
+    })
   }
 
 }
